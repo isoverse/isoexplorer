29032eb464d73ed3824ff4da4022d51d55efe617
Sebastian Kopf <seb.kopf@gmail.com>
2026-07-13 11:29:56 -0600

    update example app

--- dev/app.R
+++ dev/app.R
@@ -3,6 +3,26 @@
 # to use: run devtools::load_all("..") then call ie_explore_isofiles()
 devtools::load_all("..")
 
-library(isoreader2)
+## files single app
+# library(isoreader2)
+# example_files <-
+#   ir_examples_folder() |>
+#   ir_find_isofiles() |>
+#   ir_read_isofiles()
 
-ie_start_isofiles_server(options = list(port = 5558), upload_folder = "data")
+# # for testing purposes, always use launch = FALSE land detach = FALSE, use log_level trace to get all info
+# example_files |>
+#   ie_explore_continuous_flow(
+#     options = list(port = 5558),
+#     launch = FALSE,
+#     detach = FALSE,
+#     log_level = "TRACE"
+#   )
+
+# files server
+ie_create_isofiles_server(
+  options = list(port = 5558),
+  upload_folder = "data",
+  temporary_storage = TRUE,
+  max_upload_size = 500 * 1024^2 # allow up to 500 MB files
+)
